50bc069ac5005564a24064f062ad28bc189e08d3
mskian <veerfun23@gmail.com>
2019-10-16 23:07:07 +0530

    Netlify Status badge

--- README.md
+++ README.md
@@ -1,5 +1,7 @@
 # Tamil Quotes Images
 
+[![Netlify Status](https://api.netlify.com/api/v1/badges/182731b7-71de-4c7e-add3-a73907e60f34/deploy-status)](https://app.netlify.com/sites/tamilquotes/deploys) [![dependencies Status](https://david-dm.org/mskian/tamil-quotes/status.png)](https://david-dm.org/mskian/tamil-quotes)  
+
 Nuxt.js 🔥 + vue.js 🦄 + Ghost Content API 📦
 
 ## Starter Pack
--- components/Footer.vue
+++ components/Footer.vue
@@ -4,9 +4,10 @@
       <p>
         <strong>Made</strong> by <a href="https://www.facebook.com/sandyarmy" target="_blank" rel="nofollow noopener">Santhosh Veer</a>
       </p>
-      <p>
-        Nuxt.js 🔥 + vue.js 🦄 + Ghost Content API 📦
-      </P>
+      <p>Nuxt.js 🔥 + vue.js 🦄 + Ghost Content API 📦</P>
+      <figure class="is-flex is-inline-block">
+        <img src="https://api.netlify.com/api/v1/badges/182731b7-71de-4c7e-add3-a73907e60f34/deploy-status" alt="Tamil Quotes">
+      </figure>
     </div>
   </footer>
 </template>
