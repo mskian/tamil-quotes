d03b40abe2e5a59659f929fc3f89e556b3138e7c
mskian <veerfun23@gmail.com>
2019-10-29 14:58:22 +0530

    📝 Update Footer

--- components/Footer.vue
+++ components/Footer.vue
@@ -2,7 +2,7 @@
   <footer class="footer" itemtype="https://schema.org/WPFooter" itemscope>
     <div class="content has-text-centered">
       <p>
-        <strong>Made</strong> by <a href="https://www.facebook.com/sandyarmy" target="_blank" rel="nofollow noopener">Santhosh Veer</a> Author of <a href="https://tamilsms.blog" target="_blank" rel="nofollow noopener">tamilsms.blog</a>
+        🔮 Crafted by <a href="https://www.facebook.com/sandyarmy" target="_blank" rel="nofollow noopener">Santhosh Veer</a> 💁 Author of <a href="https://tamilsms.blog" target="_blank" rel="nofollow noopener">tamilsms.blog</a>
       </P>
     </div>
   </footer>
